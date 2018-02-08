75f3f051aa961ac1ea76b2e78c355def8c9c1f19
Serhiy K <mrlanu@gmail.com>
2018-02-08 14:47:53 -0600

    init commit

---
+++ src/main/java/com/lanu/user_front_online_banking/UserFrontOnlineBankingApplication.java
@@ -0,0 +1,12 @@
+package com.lanu.user_front_online_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFrontOnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserFrontOnlineBankingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lanu/user_front_online_banking/UserFrontOnlineBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lanu.user_front_online_banking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserFrontOnlineBankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
